a59848e301cfab07b1a25e20caa2b3a6dfe5ee8e
Gilbert526 <gilbert2004526@gmail.com>
2026-03-10 17:01:41 +0000

    Removed git tracking of compile files

--- out/build/gcc-arm/CMakeFiles/Motor_Drive.dir/startup_stm32h725xx.s.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles\Motor_Drive.dir\startup_stm32h725xx.s.obj: \
- C:\Users\Gilbert\OneDrive\ -\ The\ University\ of\ Manchester\40330.\ Team\ Project\Repository\Motor_Drive\startup_stm32h725xx.s
